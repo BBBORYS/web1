ae2a2203012908595be97ff1a543afa505e02cd6
BORYS <94471361+BBBORYS@users.noreply.github.com>
2023-09-21 21:54:24 -0500

    Update interfaces.ts

--- tarea1/interfaces.ts
+++ tarea1/interfaces.ts
@@ -1,16 +1,7 @@
-
 export interface Sale {
   id: number;
   serial: string;
   customer: string;
   printerModel: string;
   price: number;
-  saleDate: Date; 
-}
-
-export interface Printer {
-  id: number;
-  model: string;
-  brand: string;
-  stock: number; 
 }
